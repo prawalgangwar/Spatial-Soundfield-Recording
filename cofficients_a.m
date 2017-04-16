65a31e5601d511e151647ef1de161d4293e8e996
Prawal Gangwar <prawal.gangwar@gmail.com>
2017-04-16 23:10:56 +0530

    something

--- cofficients_a.m
+++ cofficients_a.m
@@ -1,10 +1,28 @@
-function [ A ] = cofficients_a(m, n, q, r_q, phi_q, k)
-    B = ones(2*n+1, 1);             %temp. resort
-    T = zeros(2*m*q+q, 2*n+1);
-    for i = -m:m
-        for j = n:-n
-           T(i+m+1, j-n+1) = besselj(i+j, k*r_q(i+m+1))*exp(-1i*(i+j)*phi_q(i+m+1));
+function [ A ] = cofficients_a(N, sam_R, sam_P, mic_r, mic_p)
+    sam_x = sam_R .* cos(sam_P);
+    sam_y = sam_R .* sin(sam_P);
+    
+    sp = zeros(N);
+    for n = 1:N
+        sp(n) = soundfield(f, sam_R(n), sam_P(n));
+    end
+    
+    mic_x = mic_r * cos(mic_p);
+    mic_y = mic_r * sin(mic_p);
+    
+    %Matrix Rotation and Translation
+    [rt_x, rt_y] = rotate_translate(sam_x, sam_y, mic_x, mic_y);
+    
+    r_x = sqrt(rt_x * rt_x + rt_y * rt_y);
+    r_p = atan(rt_y/rt_x);
+
+    J = zeros(N,N);
+    
+    for n = 1:N
+        for j = 1:N
+            J(n,j) = besselj(n, k*r_x) * exp(1i*n*r_p);
         end
     end
-    A = T * B;
+    
+    A = S * inv(J);
 end
--- transform_b.m
+++ transform_b.m
@@ -1,10 +1,8 @@
-function [ A ] = cofficients_b(m, n, q, r_q, phi_q)
+function [ T_E ] = transform_b(m, n, q, r_q, phi_q)
     T_E = zeros(2*m*q+q, 2*n+1);
-    D = ones(2*n+1, 1);             %temp. resort
     for i = -m:m
         for j = n:-n
            T_E(i+m+1, j-n+1) = besselh(i+j, k*r_q(i+m+1))*exp(-1i*(i+j)*phi_q(i+m+1)); 
         end
     end
-    A = T_E * D;    
 end
